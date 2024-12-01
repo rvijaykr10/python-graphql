f6d082e905002d89557a158070470b9c7875f5d4
vijay <vijay@vijays-MacBook-Pro.local>
2024-12-01 16:25:40 +0530

    books crud operations added

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from typing import List
+from typing import List, Optional
 from strawberry.fastapi import GraphQLRouter
 import uvicorn
 
@@ -39,6 +39,12 @@ class BookType:
     author: str
 
 
+@strawberry.input
+class BookInput:
+    title: str
+    author: str
+
+
 @strawberry.type
 class Query:
     @strawberry.field
@@ -49,8 +55,50 @@ class Query:
             BookType(id=book.id, title=book.title, author=book.author) for book in books
         ]
 
+    @strawberry.field
+    def book_by_id(self, id: int) -> Optional[BookType]:
+        db = next(get_db())
+        book = db.query(Book).filter(Book.id == id).first()
+        if book:
+            return BookType(id=book.id, title=book.title, author=book.author)
+        return None
 
-schema = strawberry.Schema(query=Query)
+
+@strawberry.type
+class Mutation:
+    @strawberry.mutation
+    def create_book(self, input: BookInput) -> BookType:
+        db = next(get_db())
+        new_book = Book(title=input.title, author=input.author)
+        db.add(new_book)
+        db.commit()
+        db.refresh(new_book)
+        return BookType(id=new_book.id, title=new_book.title, author=new_book.author)
+
+    @strawberry.mutation
+    def update_book(self, id: int, input: BookInput) -> Optional[BookType]:
+        db = next(get_db())
+        book = db.query(Book).filter(Book.id == id).first()
+        if book:
+            book.title = input.title
+            book.author = input.author
+            db.commit()
+            db.refresh(book)
+            return BookType(id=book.id, title=book.title, author=book.author)
+        return None
+
+    @strawberry.mutation
+    def delete_book(self, id: int) -> bool:
+        db = next(get_db())
+        book = db.query(Book).filter(Book.id == id).first()
+        if book:
+            db.delete(book)
+            db.commit()
+            return True
+        return False
+
+
+schema = strawberry.Schema(query=Query, mutation=Mutation)
 
 app = FastAPI()
 
@@ -60,3 +108,43 @@ app.include_router(graphql_app, prefix="/graphql")
 if __name__ == "__main__":
     Base.metadata.create_all(bind=engine)
     uvicorn.run(app, host="127.0.0.1", port=8000)
+
+
+"""
+mutation {
+  createBook(input: { title: "New Book", author: "Author Name" }) {
+    id
+    title
+    author
+  }
+}
+
+mutation {
+  updateBook(id: 1, input: { title: "Updated Title", author: "Updated Author" }) {
+    id
+    title
+    author
+  }
+}
+
+mutation {
+  deleteBook(id: 1)
+}
+
+query {
+  books {
+    id
+    title
+    author
+  }
+}
+
+query {
+  bookById(id: 1) {
+    id
+    title
+    author
+  }
+}
+
+"""
